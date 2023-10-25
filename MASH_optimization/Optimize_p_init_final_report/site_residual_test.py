fcdce51b6f75ecfe1416aa73c6b4e36ed1c2d853
chloe-cyc <chloechan32@gmail.com>
2023-10-25 16:34:18 +0100

    To Debug

--- MASH_optimization/Optimize_p_init_final_report/site_residual_test.py
+++ MASH_optimization/Optimize_p_init_final_report/site_residual_test.py
@@ -2,7 +2,6 @@
 from optimizer_find_windows import optimize
 import numpy as np
 import pandas as pd
-import time
 
 column_names = ["Time"]
 site_data_path = "/u/dem/kebl6911/Part-II/MASH_optimization/Data/mash_site300K.dat"
@@ -13,28 +12,27 @@ for i in range(1,8):
 site_df = pd.read_csv(site_data_path, delimiter=" ", names=column_names)
 site_df = site_df[(site_df.index % 10 == 0) | (site_df.index == len(site_df.index) - 1)]
 site_eq = site_df.values
-site_df = site_df[site_df["Time"] <=1000]
+full_time = site_df["Time"].values
+site_df = site_df[site_df["Time"]<=1000]
 site_values = site_df.values
 site_full = site_values[:,1:]
 
 #Extracting the equilibrium population and full length of time for this data
 eq_pop = site_eq[-1][1:8]
-full_time = site_df["Time"].values
+#full_time = full_time[full_time<=3000]
 
 #initial guess for kappa and p
 no_states = 7#number of states
 num_elements = no_states*(no_states-1)//2
 prev_kap= np.full(num_elements,0.01) 
 
-
 #Start time - cut data after this time
 #start_time_values = np.array(site_df['Time'].iloc[4::5]) 
 start_time_values = np.array(site_df['Time'])
 t_one=800
-start_time_values = start_time_values[start_time_values<t_one]
-print(start_time_values[-1])
+# t_two = 1000
+start_time_values = start_time_values[start_time_values<=t_one]
 start_time_values = start_time_values[:-5]
-print(start_time_values[-1])
 residual_sum = []
 
 for index, start_time in enumerate(start_time_values, start=1):
@@ -43,12 +41,12 @@ for index, start_time in enumerate(start_time_values, start=1):
     site_data = site_values[site_values[:,0] >= start_time]
     initial_guess_p = site_data[0,:]
     initial_guess_p = initial_guess_p[1:]
-    residual_sum_val, optimized_data, prev_kap_1 = optimize(site_data, prev_kap, initial_guess_p, eq_pop, t_one)
+    residual_sum_val, optimized_data, prev_kap_1 = optimize(site_data, prev_kap, initial_guess_p, eq_pop, full_time,t_one) #t_one,t_two, full_time)
     prev_kap = prev_kap_1
     residual_sum.append(residual_sum_val)
 
     #kappa_saved.append(index, least_squares_result)
-    np.savetxt(f"new_site/{start_time}_{index}.dat", optimized_data, delimiter="\t")
+    np.savetxt(f"new_site4/{start_time}_{index}.dat", optimized_data, delimiter="\t")
 
 residual_data = np.column_stack((start_time_values,residual_sum))
-np.savetxt(f"site_residual.dat", residual_data,delimiter="\t")
+np.savetxt(f"site_residual4.dat", residual_data,delimiter="\t")
--- MASH_optimization/optimizer_find_windows.py
+++ MASH_optimization/optimizer_find_windows.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.linalg import expm
 from scipy.optimize import least_squares
 
-def optimize(data, initial_kappas, initial_p_zero, eq_pop, t_one): #full_time, full_population
+def optimize(data, initial_kappas, initial_p_zero, eq_pop, full_time, t_one): #t_one, t_two, full_time): #full_time, full_population
 
     def listkap_matkappa(kappa):
         matkappa = np.zeros((no_states,no_states))
@@ -31,60 +31,57 @@ def optimize(data, initial_kappas, initial_p_zero, eq_pop, t_one): #full_time, f
         r_of_t = matkappa_matr(kappa)
         exp_r_del_t = expm(r_of_t*delta_t)
         p_model_result = p_model(exp_r_del_t,p_zero)
-
         residual = (abs(population_data -p_model_result)).flatten()
-        # residual = population_data -p_model_result
         return residual
+
+    def p_model_split(params):
+        kappa,p_zero = params[:no_kappas], params[no_kappas:]
+        r_of_t = matkappa_matr(kappa)
+        t_diff = t_one-time[0]
+        new_p_zero = expm(r_of_t*t_diff).dot(p_zero)
+        return new_p_zero
 
     #Inputs 
     time = data[:,0]
     delta_t = time[1]-time[0]
-    population_data = data[:,1:]
-    no_states = len(population_data[0])
+    no_states = len(data[0])-1
     if np.any(initial_p_zero<0):
         initial_p_zero = np.zeros_like(initial_p_zero)
     params = np.concatenate((initial_kappas,initial_p_zero))
     
-    
-    # time_size = time.size # The optimization is only for data up to this point
-    # data_size = time_size
     no_kappas = (no_states*(no_states-1))//2
+    # population_data = data[:,1:]
+    # time_size = time.size
+    
     train_set = data[data[:,0] <= t_one]
     train_time = train_set[:,0]
     time_size = train_time.size
     population_data = train_set[:,1:]
-    
-    test_set = data[data[:,0] >= t_one]
-    test_time = test_set[:,0]
 
     #Optimization
     least_squares_result = least_squares(residuals, params, bounds=(0,float("inf")), verbose=0, xtol=None, ftol=1e-8) # bounds=(-float("inf"),1e-17)
     optimized_kappa = least_squares_result.x[:no_kappas]
     optimized_pzero = least_squares_result.x[no_kappas:]
-    
-    #This is a bit of a hack - so don't actually do this.
-    
+    optimized_params = least_squares_result.x
+
     r_of_t_ls = matkappa_matr(optimized_kappa)
     exp_ls_del_t = expm(r_of_t_ls*delta_t)
-
-    #full_time = full_time[:,np.newaxis] # IF we want to propogate the calculation to calculate from 0 to the full length of time that we have
-    #data_size = time.size #Only divide by window
-    #time_size = full_time.size
-    
-    optimized_t_one_population = np.array(p_model(exp_ls_del_t,optimized_pzero))
-    optimized_pzero = optimized_t_one_population[-1]
-
+    test_set = data[data[:,0] >= t_one]
+    test_time = test_set[:,0]
     time_size = test_time.size
-    optimized_population = np.array(p_model(exp_ls_del_t,optimized_pzero))
-    optimized_params = np.concatenate((optimized_kappa,optimized_pzero))
     population_data = test_set[:,1:]
-    time_size = test_time.size
-    test_time = test_time[:,np.newaxis] #We propogate the calculation that we have up to the time that we have
     
-    # loss = sum(residuals(optimized_kappa)**2)
-    # print(least_squares_result.status)
-    residual = (sum(residuals(optimized_params))/time_size)/no_states
-    optimized_population = np.concatenate((test_time, optimized_population), axis=1)
-
+    #p_zero = p_model(exp_ls_del_t,optimized_pzero)[-1]
+    #New p_0 for calculation of residuals from t_one to t_two
+    p_zero = p_model_split(optimized_params)
+    optimized_params = np.concatenate((optimized_kappa, p_zero))
+    residual = sum(residuals(optimized_params))/time_size#/no_states # Calculate the residual between t_1 and t_2 only
+    
+    #full_time = full_time[full_time[:]<=t_one]
+    full_time = full_time[full_time[:] >=t_one]
+    full_time = full_time[:,np.newaxis] # IF we want to propogate the calculation to calculate from 0 to the full length of time that we have
+    time_size = full_time.size
+    optimized_population = np.array(p_model(exp_ls_del_t,p_zero))
+    optimized_population = np.concatenate((full_time, optimized_population), axis=1)
 
     return residual, optimized_population, optimized_kappa
