6e31bec2cf2c05a5b16e22c98b7dcb293ff0d102
Annina Zoe Lieberherr <chem1614@thanatos.chem.ox.ac.uk>
2023-11-07 15:05:37 +0000

    Optimizer module

--- MASH_optimization/optimizer_optimated.py
+++ MASH_optimization/optimizer_optimated.py
@@ -2,91 +2,103 @@ import numpy as np
 from scipy.linalg import expm
 from scipy.optimize import least_squares
 
-def runmodel(kappas, p_init, time):
-    
-    """
-    kappas: kappa values for the current model, 1D array
-    p_init: Initial population at time[0], 1D array of size N
-    time: times at which to calculate populations
-    """
-    n = len(kappa_size)
-    kappa_size = (n*(n-1))/2
-    if kappa_size != len(kappas):
-        return "Incompatible sizing"
-    
-    else:
+class Optimizer:
+    def __init__(self, data, eq_pop, ns, initial_kappas=None):
+        ''' Initialise the optimizer.
+        data            data to be fit
+        eq_pop          equilibrium populations
+        ns              number of states
+        initial_kappas  initial kappas (if unsure, leave None)
+        '''
+        assert len(eq_pop) == ns
+        assert data.shape[1] == ns+1
+        if(initial_kappas is None):
+            self.initial_kappas = np.zeros(ns*(ns-1)//2)
+        else:
+          assert len(initial_kappas)==ns*(ns-1)/2
+          self.initial_kappas = initial_kappas
 
-        return 
-
-
-class optimizer:
-    def __init__(self, data, initial_kappas=None, eq_pop, full_time): #full_time, full_population
-        self.data = data
-        self.initial_kappas = initial_kappas
+        data = data[::-1,:]
+        self.time = data[:,0]
+        self.pop = data[:,1:8]
         self.eq_pop = eq_pop
-        self.full_time = full_time
+        self.ns = ns
+        self.time_size = len(self.time)
+        self.dt = self.time[1]-self.time[0]
+        self.p0 = self.pop[0]
+        self.kappa_opt = None
 
     def listkap_matkappa(self, kappa):
-        matkappa = np.zeros((no_states,no_states))
-        triangle = np.triu(np.ones((no_states, no_states), dtype=bool), k=1) 
+        ''' Calculate the kappa matrix from the list of kappas.
+        '''
+        matkappa = np.zeros((self.ns,self.ns))
+        triangle = np.triu(np.ones((self.ns, self.ns), dtype=bool), k=1)
         matkappa[triangle]= kappa
         matkappa += matkappa.T
         return matkappa
 
     def matkappa_matr(self, kappa):
-        matkappa = listkap_matkappa(kappa)
-        matr = np.zeros((no_states,no_states)) 
-        matr = matkappa * eq_pop[:, np.newaxis] 
+        ''' Calculate population propagator matrix R from a list of
+        kappas.
+        '''
+        matkappa = self.listkap_matkappa(kappa)
+        matr = np.zeros((self.ns,self.ns))
+        matr = matkappa * self.eq_pop[:, np.newaxis]
         rdiag = -np.sum(matr,0)
         matr = matr + np.diag(rdiag)
         return matr
 
     def p_model(self, exp_r_del_t):
-        p_model = np.zeros((time_size,no_states))
-        p_model[0,:] = init_pop
-        for i in range(1,time_size):
+        ''' Evaluate the training model.
+        '''
+        p_model = np.zeros_like(self.pop)
+        p_model[0,:] = self.p0
+        for i in range(1,self.time_size):
             p_model[i,:] = exp_r_del_t.dot(p_model[i-1,:])
         return p_model
 
     def residuals(self, kappa):
-        r_of_t = matkappa_matr(kappa)
-        exp_r_del_t = expm(r_of_t*delta_t)
-        p_model_result = p_model(exp_r_del_t)
-        residual = (population_data -p_model_result).flatten()
+        ''' Calculate the vector of residuals.
+        '''
+        r_of_t = self.matkappa_matr(kappa)
+        exp_r_del_t = expm(r_of_t*self.dt)
+        p_model_result = self.p_model(exp_r_del_t)
+        residual = (self.pop - p_model_result).flatten()
         return residual
-    
-    def optimize(self):
-        # Extraction of data
-        self.time = self.data[:,0]
-        self. population_data = self.data[:,1]
-        self.delta_t = 
-        return residual_sum_divided, optimized_population
 
-    
-    time = data[:,0]
-    population_data = data[:,1:]
-    delta_t = time[1]-time[0]
-    no_states = len(population_data[0])
-    init_pop = population_data[0]
-    time_size = time.size
-    data_size = time_size
-    
-    #Optimization
-    least_squares_result = least_squares(residuals, initial_kappas, bounds=(0,float("inf"))) # bounds=(-float("inf"),1e-17)
-    optimized_kappa = least_squares_result.x
+    def run(self):
+        ''' Run an optimization.
+        Returns
+        res       root mean square error
+        kappa_opt optimal kappa
+        '''
+        ls_result = least_squares(self.residuals,self.initial_kappas,bounds=(0,float("inf")))#,xtol=None,ftol=1e-8)
+        kappa_opt = ls_result.x
+        self.kappa_opt = kappa_opt
+        res = np.sqrt(2.*ls_result.cost/(self.time_size*self.ns))
+        return res, kappa_opt
 
-    # Calculation of state population for output
-    #time_size = full_time.size
-    r_of_t_ls = matkappa_matr(optimized_kappa)
-    exp_ls_del_t = expm(r_of_t_ls*delta_t)
-    optimized_population = np.array(p_model(exp_ls_del_t))
-    #full_time = full_time[:,np.newaxis]
-    full_time = time[:,np.newaxis]
-    
-    # loss = sum(residuals(optimized_kappa)**2)
-    # print(least_squares_result.status)
-    residual_sum_divided = sum(abs(residuals(optimized_kappa)))/data_size
-    optimized_population = np.concatenate((full_time, optimized_population), axis=1)
+    def predict(self,p0,time,direction,kappa):
+        """
+        p0: Initial population at time[0], 1D array of size N
+        direction: Which way to propagate
+            'forward': p0 corresponds to the value at time[0]
+            'back': p0 corresponds to the value at time[-1]
+        time: times at which to calculate populations
+        direction
+        """
+        if (direction == 'back'):
+            time = time[::-1]
+        assert len(kappa) is self.ns*(self.ns-1)//2
+        r = self.matkappa_matr(kappa)
+        dt = time[1]-time[0]
+        exp_rdt = expm(r*dt)
 
+        pop = np.zeros((len(time),self.ns))
+        pop[0,:] = p0
+        for i in range(1,len(time)):
+            pop[i,:] = exp_rdt.dot(pop[i-1,:])
 
-    
+        if (direction == 'back'):
+          pop = pop[::-1,:]
+        return pop
